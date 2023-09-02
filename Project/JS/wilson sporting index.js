270b2cae22525c013dfc82c6dbfe7fa2ca9524fe
talha <talha436@gmail.com>
2023-09-02 07:08:13 -0700

    first version

---
+++ Project/JS/wilson sporting index.js
@@ -0,0 +1,13 @@
+
+// nav bar//
+const menuBtn = document.getElementById('menuBtn')
+const nav = document.getElementById('nav')
+
+
+menuBtn.addEventListener('click', function () {
+    nav.classList.toggle('show')
+})
+
+// nav bar//
+
+
---
+++ Project/main.py
@@ -0,0 +1,9 @@
+import shutil
+import os
+
+
+lst = ["Athletics" , "BasketBall" , "BoxingMAA" , "Cycling" , "Footware" , "Tennies" , "Volleyball"]
+
+
+for i in lst:
+    os.mkdir(f"CategoeyPages/{i}")
